05189b7b45da7db1ab3b474fe094246e2a3fe33c
Lucas Toledo <lucas_nahueltoledo@hotmail.com>
2026-01-10 14:30:38 +0100

    logica de eliminado y completado de tareas

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ const defaultTodos = [
 function App() {
   const [todos, setTodos] = React.useState(defaultTodos); //estado de las tareas
   const [searchValue, setSearchValue] = React.useState(""); //estado del input de busqueda
-  console.log("buscan " + searchValue); //valor del input de busqueda
-
+  /*LOGICA DEL FILTRO*/
   const completedTodos = todos.filter((todo) => !!todo.completed).length; //cantidad de tareas completadas
   const totalTodos = todos.length; //cantidad total de tareas
 
@@ -30,6 +29,20 @@ function App() {
     const searchText = searchValue.toLocaleLowerCase();
     return todoText.includes(searchText); //se usa para buscar tareas
   });
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos]; //copia del array de tareas
+    const todoIndex = todos.findIndex((todo) => todo.Text === text); //busca el indice de la tarea a completar
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed; //marca la tarea como completada
+    setTodos(newTodos); //actualiza el estado de las tareas
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos]; //copia del array de tareas
+    const todoIndex = todos.findIndex((todo) => todo.Text === text); //busca el indice de la tarea a completar
+    newTodos.splice(todoIndex, 1); //marca la tarea como completada
+    setTodos(newTodos); //actualiza el estado de las tareas
+  };
 
   return (
     <div className="app">
@@ -47,6 +60,8 @@ function App() {
               key={todo.Text}
               Text={todo.Text}
               completed={todo.completed}
+              onComplete={() => completeTodo(todo.Text)}
+              onDelete={() => deleteTodo(todo.Text)}
             />
           ))}
         </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -6,8 +6,15 @@ function TodoItem(props) {
       <p className={`text ${props.completed && "text-completed"}  `}>
         {props.Text}
       </p>
-      <span className={`span ${props.completed && "span-activate"}`}>V</span>
-      <span className={`span span-delete`}>X</span>
+      <span
+        onClick={props.onComplete}
+        className={`span ${props.completed && "span-activate"}`}
+      >
+        V
+      </span>
+      <span onClick={props.onDelete} className={`span span-delete`}>
+        X
+      </span>
     </li>
   );
 }
